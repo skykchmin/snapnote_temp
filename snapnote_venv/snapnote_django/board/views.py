9d78c8efc89cbc938b3a30cd6e4cf53cdc6f1b73
skykchmin <skykchmin@naver.com>
2020-04-23 00:53:26 +0900

    서버로 파일 전송 요청 넘기기
    서버로 파일 전송 요청 넘기기
    
    [200423]

--- snapnote_venv/snapnote_django/board/views.py
+++ snapnote_venv/snapnote_django/board/views.py
@@ -6,4 +6,8 @@ def board_list(request):
     return render(request, 'board_list.html')
 
 def upload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document'] #괄호안에 들어가는거는 input으로 전해지는 html 키
+        print(uploaded_file.name)
+        print(uploaded_file.size)
     return render(request, 'upload.html')
