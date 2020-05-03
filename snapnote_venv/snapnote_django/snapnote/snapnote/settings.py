7a2594ae2864619bb9af1e5286bcc4934a18ea2a
skykchmin <skykchmin@naver.com>
2020-05-03 23:14:12 +0900

    Pillow 및 crispy form 적용
    Pillow 및 crispy form 적용
    
    [200503]

--- snapnote_venv/snapnote_django/snapnote/snapnote/settings.py
+++ snapnote_venv/snapnote_django/snapnote/snapnote/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'crispy_forms',
     'snuser',
     'board',
     'file',
@@ -126,3 +128,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
