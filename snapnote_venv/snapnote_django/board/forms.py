d949f93788ed08717cd20fbd45d1d7ca1f2cd00c
skykchmin <skykchmin@naver.com>
2020-05-27 23:20:52 +0900

    FileForm, 각종화면 처리 , 다운로드 폼
    FileForm, 각종화면 처리 , 다운로드 폼
    
    [200527]

--- snapnote_venv/snapnote_django/board/forms.py
+++ snapnote_venv/snapnote_django/board/forms.py
@@ -3,6 +3,13 @@ from django import forms
 from .models import File
 
 class FileForm(forms.ModelForm):
+    title = forms.CharField(
+        error_messages={
+            'required': '제목을 입력해주세요'    
+        }, 
+        max_length=128, label="제목")
+    pdf = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
     class Meta:
         model = File
         fields = {'title', 'pdf'}
--- snapnote_venv/snapnote_django/board/views.py
+++ snapnote_venv/snapnote_django/board/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
 
+import os
 from .forms import FileForm
 from .models import File
+
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -28,14 +31,75 @@ def file_list(request):
         'files': files
     })
 
+# def upload_file(request):
+#     if request.method == 'POST':
+#         form = FileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('file_list')
+#     else:
+#         form = FileForm()
+#     return render(request,'upload_file.html', {
+#         'form': form
+#     })
+
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         form = FileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(os.getcwd())
+#             for count, x in enumerate(request.FILES.getlist("files")):
+#                 def handle_uploaded_file(file):
+#                     with open(os.path.join(os.getcwd(),"media", file.name),'wb+') as destination:
+#                         for chuck in file.chucks():
+#                             destination.write(chuck)
+#                 handle_uploaded_file(x)
+#             # form.save()
+#             return redirect('file_list')
+#     else:
+#         form = FileForm()
+#         print(os.getcwd())
+#     return render(request,'upload_file.html', {
+#         'form': form
+#     })
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         form = FileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             for count, x in enumerate(request.FILES.getlist("files")):
+#                 def handle_uploaded_file(f):
+#                     with open(r'C:\VSCODE\django\snapnote_venv\snapnote_django\snapnote\media\files\pdfs\file_' + str(count), 'wb+') as destination:
+#                         for chuck in f.chucks():
+#                             destination.write(chuck)
+#                 handle_uploaded_file(x)
+#             # form.save()
+#             # return redirect('file_list')
+#             return HttpResponse("파일 업로드!")
+#     else:
+#         form = FileForm()
+#     return render(request,'upload_file.html', {
+#         'form': form
+#     })
+
 def upload_file(request):
     if request.method == 'POST':
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('file_list')
+            for count, x in enumerate(request.FILES.getlist("files")):
+                def handle_uploaded_file(f):
+                    with open('C:\VSCODE\django\snapnote_venv\snapnote_django\snapnote\media\files\pdfs\file_' + str(count), 'wb+') as destination:
+                        for chuck in f.chucks():
+                            destination.write(chuck)
+                handle_uploaded_file(x)
+            print(os.getcwd())
+            # form.save()
+            # return redirect('file_list')
+            return HttpResponse("파일 업로드!")
     else:
         form = FileForm()
     return render(request,'upload_file.html', {
         'form': form
     })
+    
